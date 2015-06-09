ff165176316e743f7ea312982fa48f5fc025668a
rafael.santos <rafael.santos@taqtile.com>
2015-06-09 17:32:15 -0300

    created book and author classes

---
+++ Author.java
@@ -0,0 +1,10 @@
+public class Author {
+	String name;
+	String email;
+
+	void showDetail() {
+		System.out.println("Author details");
+		System.out.println("Name: " + name);
+		System.out.println("Email: " + email);
+	}
+}
